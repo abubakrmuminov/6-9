f3e86f18f52539d41760737f6781ae82b449008e
Abubakr <170409v@gmail.com>
2025-07-15 15:46:46 +0500

    i

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
-import React from "react"; // ты забыл импортировать React!
-import ReactDOM from "react-dom/client"; // и ReactDOM тоже!
+import React from "react";
+import ReactDOM from "react-dom/client"; 
 import "./index.css";
 import App from "./App";
 import { AuthProvider } from "./AuthContext";
